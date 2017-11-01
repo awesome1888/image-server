8406c12b4ca0e32dab30446aab850da2459cd601
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-11-01 21:01:45 +0100

    cache

--- application/index.js
+++ application/index.js
@@ -44,29 +44,69 @@ class Application extends BaseApplication
         const size = this.extractSize(req);
         if (size)
         {
-            try
-            {
-                sharp(path)
-                    .resize(size[0], size[1])
-                    .jpeg({
-                        quality: 91,
-                    })
-                    .toBuffer()
-                    .then((data) => {
-                        this.setContentTypeHeader(res);
-                        res.end(data);
-                    })
-                    .catch(() => {
-                        this.send404(res);
+            this.streamResized(file, size, res).catch(() => {
+                // not found, create and try to stream again
+                try
+                {
+                    this.resize(file, size).then((cachedPath) => {
+                        // send cached copy, if okay
+                        this.streamFile(cachedPath, res);
+                    }).catch((e) => {
+                        console.dir(e);
+                        this.send500(res);
                     });
-            } catch(e) {
-                this.send500(res);
-            }
+                } catch(e) {
+                    this.send500(res);
+                }
+            });
         }
         else
         {
             this.streamFile(path, res);
         }
+    }
+
+    streamResized(file, size, res)
+    {
+        return this.prepareCachePath().then(() => {
+            return this.prepareCacheSubPath(file);
+        }).then(() => {
+            const cPath = this.getCachedFilePath(file, size);
+
+            return new Promise((resolve, reject) => {
+                const stream = fs.createReadStream(cPath);
+                stream.on('error', (e) => {
+                    reject(e);
+                });
+                stream.on('open', () => {
+                    this.setCacheControlHeaders(res);
+                    resolve();
+                });
+
+                stream.pipe(res);
+            });
+        });
+    }
+
+    resize(file, size)
+    {
+        const pathFrom = this.getFilePath(file);
+        const pathTo = this.getCachedFilePath(file, size);
+
+        return new Promise((resolve, reject) => {
+            sharp(pathFrom).resize(size[0], size[1]).jpeg({
+                quality: 91,
+            }).toFile(pathTo, (err, info) => {
+                if (err)
+                {
+                    reject(err);
+                }
+                else
+                {
+                    resolve(pathTo);
+                }
+            });
+        });
     }
 
     extractSize(req)
@@ -121,6 +161,78 @@ class Application extends BaseApplication
     {
         return this.getSettings().storagePath || '';
     }
+
+    getCachePath()
+    {
+        return this.getSettings().cachePath || '';
+    }
+
+    getCachedSubPath(file)
+    {
+        const storage = this.getCachePath();
+
+        file = file.replace(/\.+/g, '.');
+        const subFolder = file.substr(0, 3);
+
+        return `${storage}/${subFolder}/`;
+    }
+
+    getCachedFilePath(file, size)
+    {
+        const subPath = this.getCachedSubPath(file);
+
+        return `${subPath}/${file}_${size[0]}x${size[1]}`;
+    }
+
+    prepareCacheSubPath(file)
+    {
+        const path = this.getCachedSubPath(file);
+        if (!_.isStringNotEmpty(path))
+        {
+            throw new Error();
+        }
+
+        return this.makeFolder(path).catch(x => x);
+    }
+
+    prepareCachePath()
+    {
+        const path = this.getCachePath();
+        if (!_.isStringNotEmpty(path))
+        {
+            throw new Error();
+        }
+
+        return this.makeFolder(path).catch(x => x);
+    }
+
+    // fs
+    // todo: move to a separate module
+    makeFolder(folder)
+    {
+        return new Promise((resolve, reject) => {
+            fs.mkdir(folder, 0o755, (err) => {
+                if (err)
+                {
+                    reject(err);
+                }
+                else
+                {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    // static isExists(folder)
+    // {
+    //     return new Promise((resolve) => {
+    //         fs.stat(folder, (err) => {
+    //             // todo: poor check
+    //             resolve(!err);
+    //         });
+    //     });
+    // }
 }
 
 module.exports = Application;
--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
     port: 3012,
     storagePath: '/home/sergey/upload/',
+    cachePath: '/home/sergey/.image-server-cache/',
     cacheTime: 3600,
     availableSizes: [
         [1000, 1000],
         [120, 120], // admin previews
     ],
+    // todo: implement cache ttl
 };
