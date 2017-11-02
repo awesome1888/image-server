8e3968c2f60a829d05c4ed405f2c7f6a629f3d21
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-11-02 16:16:24 +0100

    workers added

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
 let Application = require('./application/index.js');
 
-(new Application()).launch();
+if (cluster.isMaster)
+{
+    // create workers
+    console.dir(`Cpu num: ${numCPUs}`);
+    for (let i = 0; i < numCPUs; i++)
+    {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    // creating worker application, it will share all tcp connections
+    // https://linuxtrainers.wordpress.com/2014/12/31/how-fork-system-call-works-what-is-shared-between-parent-and-child-process/
+    (new Application()).launch();
+}
