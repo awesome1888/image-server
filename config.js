7ac725a53190171d590a8a6dc2c897976b951d01
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-11-10 23:00:17 +0100

    chain response

--- config.js
+++ config.js
@@ -5,6 +5,14 @@ module.exports = {
     cacheTime: 3600,
     legalSizes: [
         [900, 900], // just for testing
+
+        // article detail
+        [900, 200],
+        [900, 300],
+        [800, 200],
+        [400, 200],
+        [200, 200],
+
         [100, 100], // admin previews
         [10, 10], // for fun
     ],
