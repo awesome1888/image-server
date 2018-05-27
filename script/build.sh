0844ad55460449b7dcd188e5f9e7742a1139e73f
Sergei <gannochenko.sv@gmail.com>
2018-05-28 00:03:42 +0200

    production dockerfile

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VERSION="${1:-latest}"
 
+# build backend
 cd $DIR/../app/backend/;
-docker build -t awesome1888/image-server:latest .;
-docker push awesome1888/image-server:latest;
+docker build -f docker/production.docker -t awesome1888/image-server:$VERSION .
+docker push awesome1888/image-server:$VERSION
+docker rmi awesome1888/image-server:$VERSION
+
